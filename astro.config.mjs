0665fff1d36c70335055a5e76aa7e57864670210
Stefan le Roux <stefan@vivtec.co.za>
2026-01-24 22:38:56 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://vivtec.github.io",
+	site: "https://vivtec-net.github.io/web-vivtec-main/",
 });
