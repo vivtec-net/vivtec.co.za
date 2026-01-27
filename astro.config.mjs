2e45f62d7748e15e5a9758b0f659e80c292f4068
Stefan le Roux <stefan@vivtec.co.za>
2026-01-27 20:48:55 +0200

    Meta prop update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://vivtec.co.za",
-	integrations: [mdx()],
+	integrations: [mdx(), sitemap()],
 });
